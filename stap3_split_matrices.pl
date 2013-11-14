65fac3d714a2e09bf55c5b727ba44b57cc58ae9c
Nick Overdijk <nick@astrant.net>
2013-11-14 14:28:24 +0100

    Merges stap4 into stap3

--- stap3_split_matrices.pl
+++ stap3_split_matrices.pl
@@ -6,21 +6,24 @@ use warnings;
 my @regel;
 my $naam = "ng1_PCA";
 
-open(FILE, "${naam}.txt");
-while (my $line = <FILE>)
-{
-  chomp $line;
-  @regel = split(/,/,$line);
-  foreach my $i (1,5,10,20,50,100,200){
-	maak_matrix($i);
-  }
+foreach my $i (1,5,10,20,50,100,200){
+	open(FILE, "${naam}.txt");
+	my $linecount = 1;
+	open my $output_file, ">${naam}_$i.txt" or die "...$!";
+	while (my $line = <FILE>) {
+		chomp $line;
+		@regel = split(/,/,$line);
+		maak_matrix($i, $linecount, $output_file);
+		$linecount++;
+	}
 }
 
 sub maak_matrix
 {
   my ($matrixregel,$p);
   my $count = shift;
-  open(OUT, ">>${naam}_$count.txt");
+  my $linecount = shift;
+  my $OUT = shift;
   
   $matrixregel = "";
   for ($p = 0; $p < $count; $p++)
@@ -28,5 +31,11 @@ sub maak_matrix
     $matrixregel = $matrixregel.",$regel[$p]";
   }
   $matrixregel =~ s/^,([0-9]*.*)/$1/;
-  print OUT "$matrixregel\n";
+  print $OUT "$matrixregel,";
+  if($linecount <= 320){
+  	  print $OUT "F";
+  } else {
+  	  print $OUT "M";
+  }
+  print $OUT "\n";
 }
